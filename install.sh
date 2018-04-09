151d1c85ec2d209dc2c9f165536fda9e7c588f91
GregHilston <Gregory.Hilston@gmail.com>
2018-04-09 11:25:50 -0600

    Updated install script

--- install.sh
+++ install.sh
@@ -1,7 +1,16 @@
 #!/bin/bash
 
+echo "## Creating our Git repo"
 mkdir -p ~/Git
 pushd ~/Git
 git clone https://github.com/GregHilston/kaggle-talking-data-ad-tracking-fraud-detection-challenge
-sudo apt-get install python3-pip jupyter jupyterlab htop -y
-pip3 install -r requirements.txt -y
+pushd ~/Git/kaggle-talking-data-ad-tracking-fraud-detection-challenge
+
+echo "## Updating apt's repository"
+sudo apt-get update
+
+echo "## Installing apt packages"
+sudo apt-get install build-essential tmux python3-pip htop -y
+
+echo "## Installing pip3 packages"
+pip3 install -r requirements.txt
