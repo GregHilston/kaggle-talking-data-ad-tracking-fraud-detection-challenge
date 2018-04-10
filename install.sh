684050e96ca4099ae3b6e3325ac73550cf6c032d
GregHilston <Gregory.Hilston@gmail.com>
2018-04-10 02:59:46 +0000

    Updated install script to configure git

--- install.sh
+++ install.sh
@@ -15,4 +15,8 @@ sudo apt-get install build-essential tmux python3-pip htop -y
 echo "## Installing pip3 packages"
 pip3 install -r requirements.txt
 
+echo "## Configuring Git"
+git config --global user.name "GregHilston"
+git config --global user.email "Gregory.Hilston@gmail.com"
+
 echo "## Don't forget to copy data over into the data/ directory"
